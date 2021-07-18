447a70f0ffd4c692d84b4189f008ceb31c9a3589
Nicolas Lardot <lardotn@gmail.com>
2021-07-18 14:30:49 +0200

    Create PlantModel.kt for add a list of plants

---
+++ app/src/main/java/com/youtube/naturecollection/PlantModel.kt
@@ -0,0 +1,8 @@
+package com.youtube.naturecollection
+
+class PlantModel(
+    val name: String = "Tulipe",
+    val description: String = "Petite description",
+    val imageUrl: String = "http://graven.yt/plante.jpg",
+    var liked: Boolean = false
+)
--- app/src/main/java/com/youtube/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/com/youtube/naturecollection/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.youtube.naturecollection.PlantModel
 import com.youtube.naturecollection.R
 import com.youtube.naturecollection.adapter.PlantAdapter
 import com.youtube.naturecollection.adapter.PlantItemDecoration
@@ -19,13 +20,22 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater?.inflate(R.layout.fragment_home, container, false)
 
+        // Création de la liste pour stocker les plantes
+        val plantList = arrayListOf<PlantModel>()
+
+        // Enregistrer une première plante dans la liste
+        plantList.add(PlantModel("Pissenlit","jaune soleil","https://cdn.pixabay.com/photo/2014/05/01/17/51/flower-335662_960_720.jpg",false))
+        plantList.add(PlantModel("Rose","ça pique un peu","https://cdn.pixabay.com/photo/2018/05/16/22/27/rose-3407234_960_720.jpg",false))
+        plantList.add(PlantModel("Cactus","ça pique beaucoup","https://cdn.pixabay.com/photo/2016/07/06/20/47/prickly-pear-1501307_960_720.jpg",false))
+        plantList.add(PlantModel("Tulipe","c'est beau","https://cdn.pixabay.com/photo/2017/03/13/21/19/tulip-2141216_960_720.jpg",false))
+
         // Récupérer le recyclerview
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
-        horizontalRecyclerView.adapter = PlantAdapter(R.layout.item_horizontal_plant)
+        horizontalRecyclerView.adapter = PlantAdapter(plantList, R.layout.item_horizontal_plant)
 
         // Récupérer le second recyclerview
         val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
-        verticalRecyclerView.adapter = PlantAdapter(R.layout.item_vertical_plant)
+        verticalRecyclerView.adapter = PlantAdapter(plantList, R.layout.item_vertical_plant)
         verticalRecyclerView.addItemDecoration(PlantItemDecoration())
 
         return view
