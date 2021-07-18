96bee7e030570e25955047b1b9b1335d1eb2a2d3
Nicolas Lardot <lardotn@gmail.com>
2021-07-18 12:20:40 +0200

    Create fragment_home.xml and HomeFragment.kt. Testing it whith trending pictures

--- app/src/main/java/com/youtube/naturecollection/MainActivity.kt
+++ app/src/main/java/com/youtube/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package com.youtube.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.youtube.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Injecter le fragment dans le fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
